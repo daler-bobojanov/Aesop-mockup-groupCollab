1639b9d46e727a9f74687fd49c219c7e3a6278dd
Daquan Johnson <sadu@Daquans-MBP.fios-router.home>
2020-04-15 18:27:09 -0400

    finished header, almost finished store finder

---
+++ aesop-mockup/src/components/Storefinder.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import storeImage from '../assets/images/Aesop-Bleecker-Street-Carousel-1-Desktop-2560x1440px.jpg'
+
+function Storefinder() {
+    return (
+        <div className='store-finder' >
+        <div id='left-side' >
+        <p id='AD' > Architecture and Design </p>
+        <p id='AD-info' >Spaces of distinction</p>
+        </div>
+        <div id='right-side' >
+        <p> Aesop stores are informed by the history, fabric and context of their local environments, yet retain an aesthetic consistency. </p>
+        <button id='store-button' > Find a nearby store ➝  </button>
+        </div>
+         <img id='store-images' src={storeImage} alt="stores"/>
+        </div>
+    );
+}
+
+export default Storefinder
