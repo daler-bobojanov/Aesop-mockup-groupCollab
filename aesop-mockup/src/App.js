4f6f712a5905c0e2d6f1f1071440629cdb783cbe
DALER BOBOJANOV <daler_bobojanov@DALERs-MacBook-Pro.local>
2020-04-15 19:55:02 -0400

    rendered second component

--- aesop-mockup/src/App.js
+++ aesop-mockup/src/App.js
@@ -3,13 +3,14 @@ import React from 'react';
 import DalerComponent1 from './components/DalerComponent1';
 import Gift from './components/Gift';
 import GiftSecond from './components/GiftSecond';
-
+import DalerComponent2 from './components/DalerComponent2';
 
 function App() {
   return (
     <div className="App">
       <DalerComponent1 />
       <Gift />
+      <DalerComponent2 />
       <GiftSecond />
     </div>
   );
