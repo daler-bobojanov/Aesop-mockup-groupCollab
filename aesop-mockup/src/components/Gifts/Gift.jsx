96cf58110b54b0f9e58b4807f478206f2d582211
Aleh <user@iMac-User.fios-router.home>
2020-04-14 22:01:37 -0400

    worked on responsive design

--- aesop-mockup/src/components/Gifts/Gift.jsx
+++ aesop-mockup/src/components/Gifts/Gift.jsx
@@ -5,11 +5,15 @@ export default function Gift() {
     return (
         <div className='container'>
             <div className="leftSide">
-                <p>Five mythical Gift Kits</p>
-                <p>Crafted for gods, mortals, and muses alike, our new Gift Kit <br/> collection makes for inspired gift giving this festive season.</p>
-                <button>Explore seasonal Gift Kits</button>
+                <div className="leftSide_text">
+                    <p>Five mythical Gift Kits</p>
+                    <p>Crafted for gods, mortals, and muses alike, our new Gift Kit <br/> collection makes for inspired gift giving this festive season.</p>
+                    <button>Explore seasonal Gift Kits</button>
+                </div>
             </div>
-            <div className="rightSide"></div>
+            <div className="rightSide">
+                <div className="rightSide_image"></div>
+            </div>
         </div>
     )
 }
