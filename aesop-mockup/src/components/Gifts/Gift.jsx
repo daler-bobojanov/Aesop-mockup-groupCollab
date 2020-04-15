93e1d26b54b453f9171b18a47aa2089c59da791b
Aleh <user@iMac-User.fios-router.home>
2020-04-14 21:24:02 -0400

    button added

---
+++ aesop-mockup/src/components/Gifts/Gift.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Gift.css'
+
+export default function Gift() {
+    return (
+        <div className='container'>
+            <div className="leftSide">
+                <p>Five mythical Gift Kits</p>
+                <p>Crafted for gods, mortals, and muses alike, our new Gift Kit <br/> collection makes for inspired gift giving this festive season.</p>
+                <button>Explore seasonal Gift Kits</button>
+            </div>
+            <div className="rightSide"></div>
+        </div>
+    )
+}
