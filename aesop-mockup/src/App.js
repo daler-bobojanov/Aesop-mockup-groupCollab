22360662fc4fe8edd207fc5edcfda072dcf7ae29
Aleh <user@iMac-User.fios-router.home>
2020-04-14 22:25:06 -0400

    recreated files

--- aesop-mockup/src/App.js
+++ aesop-mockup/src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
+import Gift from './Components/Gift/Gift'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello Daler, Daquan, Aleh! Code the shit out of me...</h1>
-        <h3>Ps.</h3>
-        <h2>React</h2>
-      </header>
+        <Gift />
     </div>
   );
 }
