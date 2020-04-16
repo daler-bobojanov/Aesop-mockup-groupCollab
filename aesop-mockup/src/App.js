b3bf3cf2df1f2e3bfcd20bc3499f1449dd6b0a42
DALER BOBOJANOV <daler_bobojanov@DALERs-MacBook-Pro.local>
2020-04-16 17:02:56 -0400

    minor fixes after daquan/aleh merge with head/master

--- aesop-mockup/src/App.js
+++ aesop-mockup/src/App.js
@@ -1,31 +1,25 @@
 import React from 'react';
-<<<<<<< HEAD
 
 import DalerComponent1 from './components/DalerComponent1';
 import Gift from './components/Gift';
 import GiftSecond from './components/GiftSecond';
 import GiftThree from './components/GiftThree';
 
-=======
 import Header from './components/Header';
 import Storefinder from './components/Storefinder';
 import Footer from './components/Footer';
 import './App.css';
->>>>>>> 4736d4bb23b903a4db3f31d919cd8e0ad0f182d8
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
+      <Header />
       <DalerComponent1 />
       <Gift />
       <GiftSecond />
+      <Storefinder />
       <GiftThree />
-=======
-      <Header/>
-      <Storefinder/>
-      <Footer/>
->>>>>>> 4736d4bb23b903a4db3f31d919cd8e0ad0f182d8
+      <Footer />
     </div>
   );
 }
