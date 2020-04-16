a839c280872703185fbf4e4ea5c1fc0027ea822a
Daquan Johnson <sadu@Daquans-MBP.fios-router.home>
2020-04-15 20:13:04 -0400

    finished footer

--- aesop-mockup/src/components/Footer.jsx
+++ aesop-mockup/src/components/Footer.jsx
@@ -3,39 +3,43 @@ import React from 'react';
 function Footer() {
     return (
         <div className='footer' >
-            <hr/>
-            <div id='left-side'>
-            <p>I would like to receive communications about Aesop products, services, events and matters <br/> of cultural interest.</p>
-            <input type="text" placeholder='Email adress'/>
-            </div>
-            <div id='right-side' >
-            <p>Contact us</p>
-            <ul className='contact-us' >
-                <li>Contact Us</li>
-                <li>FAQs</li>
-                <li>Delivery and Returns</li>
-                <li>Track your order</li>
-                <li>Check Gift Card Balance</li>
-                <li>Terms and Conditions</li>
-                <li>Accesibility</li>
-            </ul>
-            <p>About</p>
-            <ul className='about' >
-                <li>Our story</li>
-                <li>Careers</li>
-                <li>Press</li>
-                <li>Corporate Gifts</li>
-                <li>Facial appointments</li>
-            </ul>
-            <p>Social</p>
-            <ul>
-                <li>Instagram ↗</li>
-                <li>Twitter ↗</li>
-                <li>LinkedIn ↗</li>
-            </ul>
-            </div>
-            <div id='right-side' >
-
+            <hr id='footer-hr' />
+            <div className='lists' >
+                    <div id='email'>
+                    <p>I would like to receive communications about Aesop products, services, events and matters</p>
+                    <p id='line-two' > of cultural interest. </p>
+                    <input type="text" placeholder='Email address'/>
+                    <hr id='email-hr' />
+                    </div>
+                    
+                    <div id='links' >
+                    <p>Contact us</p>
+                    <ul className='contact-us' >
+                        <li>Contact Us</li>
+                        <li>FAQs</li>
+                        <li>Delivery and Returns</li>
+                        <li>Track your order</li>
+                        <li>Check Gift Card Balance</li>
+                        <li>Terms and Conditions</li>
+                        <li>Accesibility</li>
+                    </ul>
+                    <ul className='about' >
+                        <p>About</p>
+                        <li>Our story</li>
+                        <li>Careers</li>
+                        <li>Press</li>
+                        <li>Corporate Gifts</li>
+                        <li>Facial appointments</li>
+                    </ul>
+                    <ul className='social' >
+                        <p>Social</p>
+                       <a href="https://www.instagram.com/aesopskincare/"> <li>Instagram ↗</li> </a>
+                       <a href="https://twitter.com/aesopskincare"> <li>Twitter ↗</li> </a>
+                       <a href="https://www.linkedin.com/company/aesop"> <li>LinkedIn ↗</li> </a>
+                    </ul>
+                    <div id='geotag' ></div>
+                    </div>
+                    
             </div>
         </div>
     );
